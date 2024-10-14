37588236c8653adcf8d3262b78b31fde3cc7f8cd
VladimirGorin <vladimirgorin410@gmail.com>
2024-10-14 22:17:59 +0600

    fix bugs

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ logging.basicConfig(level=logging.INFO,
                     ])
 app = Flask(__name__)
 
+STATE_FILE = './state.json'
+
 
 class TelegramSessionManager:
     def __init__(self, sessions_dir, bad_sessions_file):
@@ -25,13 +27,11 @@ class TelegramSessionManager:
         for file in os.listdir(self.sessions_dir):
             if file.endswith('.session'):
                 session_name = file.replace('.session', '')
-                json_file = os.path.join(
-                    self.sessions_dir, f'{session_name}.json')
+                json_file = os.path.join(self.sessions_dir, f'{session_name}.json')
                 if os.path.exists(json_file):
                     sessions.append((session_name, json_file))
                 else:
-                    logging.error(f'JSON файл для сессии {
-                                  session_name} не найден.')
+                    logging.error(f'JSON файл для сессии {session_name} не найден.')
         return sessions
 
     def load_api_credentials(self, json_file):
@@ -41,8 +41,7 @@ class TelegramSessionManager:
                 app_id = data.get('app_id')
                 app_hash = data.get('app_hash')
                 if not app_id or not app_hash:
-                    raise ValueError(
-                        f'Недостаточно данных в файле {json_file}')
+                    raise ValueError(f'Недостаточно данных в файле {json_file}')
                 return app_id, app_hash
         except (FileNotFoundError, json.JSONDecodeError, ValueError) as e:
             logging.error(f'Ошибка при чтении {json_file}: {e}')
@@ -54,8 +53,7 @@ class TelegramSessionManager:
         logging.info(f'Сессия {session_name} добавлена в черный список.')
 
     def remove_session(self, session_name):
-        session_file = os.path.join(
-            self.sessions_dir, f'{session_name}.session')
+        session_file = os.path.join(self.sessions_dir, f'{session_name}.session')
         json_file = os.path.join(self.sessions_dir, f'{session_name}.json')
         if os.path.exists(session_file):
             os.remove(session_file)
@@ -68,12 +66,10 @@ class TelegramSessionManager:
         try:
             result = await client.get_entity(CHANNEL_USERNAME)
             if result.left:
-                logging.info(f'Аккаунт не подписан на канал {
-                             CHANNEL_USERNAME}, подписываемся.')
+                logging.info(f'Аккаунт не подписан на канал {CHANNEL_USERNAME}, подписываемся.')
                 await client(functions.channels.JoinChannelRequest(CHANNEL_USERNAME))
         except Exception as e:
-            logging.error(f'Ошибка при подписке на канал {
-                          CHANNEL_USERNAME}: {e}')
+            logging.error(f'Ошибка при подписке на канал {CHANNEL_USERNAME}: {e}')
             return False
         return True
 
@@ -92,8 +88,7 @@ class TelegramSessionManager:
                 await client.disconnect()
                 valid_sessions.append((session_name, json_file))
             except Exception:
-                logging.error(f'Некорректная сессия {
-                              session_name}. Удаляем сессию.')
+                logging.error(f'Некорректная сессия {session_name}. Удаляем сессию.')
                 self.remove_session(session_name)
                 self.add_to_bad_sessions(session_name)
         return valid_sessions
@@ -104,22 +99,18 @@ class TelegramSessionManager:
             raise Exception("Нет валидных сессий")
         result = []
         while ids:
-
             valid_sessions_count = len(valid_sessions)
             if valid_sessions_count == 0:
                 break
 
             ids_per_session = len(ids) // valid_sessions_count
-
-            ids_per_session += 1 if ids_per_session * \
-                valid_sessions_count < len(ids) else 0
+            ids_per_session += 1 if ids_per_session * valid_sessions_count < len(ids) else 0
 
             for session_name, json_file in valid_sessions:
                 if not ids:
                     break
                 try:
-                    session_file = os.path.join(
-                        self.sessions_dir, session_name)
+                    session_file = os.path.join(self.sessions_dir, session_name)
                     app_id, app_hash = self.load_api_credentials(json_file)
                     if not app_id or not app_hash:
                         raise Exception("Не авторизован")
@@ -137,16 +128,14 @@ class TelegramSessionManager:
                         for message in session_ids:
                             message_id = message["id"]
                             line = message["line"]
-                            logging.info(f"Отправляем ID: {
-                                         message_id} через сессию {session_name}.")
+                            logging.info(f"Отправляем ID: {message_id} через сессию {session_name}.")
                             await client.send_message(TARGET_BOT, message_id)
                             await asyncio.sleep(7)
                             async for response in client.iter_messages(TARGET_BOT, limit=1):
                                 result.append({"text": response.text, "line": str(line)})
 
                 except errors.AuthKeyUnregisteredError:
-                    logging.error(f'Необходимо ввести телефон или токен для сессии {
-                                  session_name}. Удаляем сессию.')
+                    logging.error(f'Необходимо ввести телефон или токен для сессии {session_name}. Удаляем сессию.')
                     self.remove_session(session_name)
                     self.add_to_bad_sessions(session_name)
                 except (errors.SessionRevokedError, errors.PhoneCodeExpiredError, errors.FloodWaitError) as e:
@@ -154,8 +143,7 @@ class TelegramSessionManager:
                     self.remove_session(session_name)
                     self.add_to_bad_sessions(session_name)
                 except Exception as e:
-                    logging.error(f'Неожиданная ошибка с сессией {
-                                  session_name}: {e}')
+                    logging.error(f'Неожиданная ошибка с сессией {session_name}: {e}')
 
         return result
 
@@ -163,8 +151,28 @@ class TelegramSessionManager:
 session_manager = TelegramSessionManager('./sessions', './bad_sessions.txt')
 
 
-@app.route('/api/get_info', methods=['POST'])
-async def get_info():
+def save_state_to_file(data):
+    try:
+        with open(STATE_FILE, 'w') as f:
+            json.dump(data, f)
+        logging.info(f"Состояние успешно сохранено в {STATE_FILE}.")
+    except Exception as e:
+        logging.error(f"Ошибка при сохранении состояния: {e}")
+
+
+def load_state_from_file():
+    try:
+        if os.path.exists(STATE_FILE):
+            with open(STATE_FILE, 'r') as f:
+                return json.load(f)
+        return {}
+    except Exception as e:
+        logging.error(f"Ошибка при загрузке состояния: {e}")
+        return {}
+
+
+@app.route('/api/process_ids', methods=['POST'])
+async def process_ids():
     data = request.get_json()
     if 'ids' not in data:
         logging.error('Поле "ids" обязательно.')
@@ -174,8 +182,21 @@ async def get_info():
     if not sessions:
         logging.error('Нет доступных сессий.')
         return jsonify({'error': 'Нет доступных сессий.'}), 400
+
     result = await session_manager.send_messages_to_bot(ids, sessions)
+
+    state_data = load_state_from_file()
+    state_data['last_result'] = result
+    save_state_to_file(state_data)
+
     return jsonify({'result': result})
+
+
+@app.route('/api/get_ids', methods=['GET'])
+def get_ids():
+    state_data = load_state_from_file()
+    return jsonify(state_data)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
